ab2a6060ee62a321b489f224c115ce1ee4c97768
brandon-sew <164340245+brandon-sew@users.noreply.github.com>
2026-04-15 14:54:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -186,7 +186,7 @@ if os.path.exists(geojson_folder):
 
         fig_map.update_layout(
             # 0 margin ensures the grey box touches the dashboard edges
-            margin={"r":1,"t":0,"l":1,"b":0},
+            margin={"r":0,"t":0,"l":0,"b":0},
             height=1000, 
             coloraxis_showscale=False,
             paper_bgcolor="#f0f2f6",
